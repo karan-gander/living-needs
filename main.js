3ada35d7ffd2e5d81f236736e56683c0bae7b61c
karan-gander <meghawalkaran@gmail.com>
2024-12-24 19:39:11 +0530

    start on workig contact page

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 
-let header = document.getElementById();
+document.addEventListener("scroll",()=>{
+    const header = document.getElementById("header")
+    if(window.scrollY>0){
+        
+        header.classList.add("scrolled")
+    }
+    else{
+        header.classList.remove("scrolled")
+    }
+})
