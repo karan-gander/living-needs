d5c5e7aeaa1f70de9e810549b27986329bb96782
karan-gander <meghawalkaran@gmail.com>
2024-12-27 15:27:48 +0530

    Error in nav

--- main.js
+++ main.js
@@ -1,11 +1,42 @@
-
 document.addEventListener("scroll",()=>{
-    const header = document.getElementById("header")
-    if(window.scrollY>0){
-        
-        header.classList.add("scrolled")
-    }
-    else{
-        header.classList.remove("scrolled")
-    }
+  const header = document.getElementById("header")
+  if(window.scrollY>0){
+      
+      header.classList.add("scrolled")
+  }
+  else{
+      header.classList.remove("scrolled")
+  }
 })
+
+
+
+
+
+const accordion = document.querySelector(".accordion");
+
+accordion.addEventListener("mouseover", (e) => {
+  const activePanel = e.target.closest(".accordion-panel");
+  if (!activePanel) return;
+  toggleAccordion(activePanel);
+});
+
+function toggleAccordion(panelToActivate) {
+  const buttons = panelToActivate.parentElement.querySelectorAll("button");
+  const contents =
+    panelToActivate.parentElement.querySelectorAll(".accordion-content");
+
+  buttons.forEach((button) => {
+    button.setAttribute("aria-expanded", false);
+  });
+
+  contents.forEach((content) => {
+    content.setAttribute("aria-hidden", true);
+  });
+
+  panelToActivate.querySelector("button").setAttribute("aria-expanded", true);
+
+  panelToActivate
+    .querySelector(".accordion-content")
+    .setAttribute("aria-hidden", false);
+}
